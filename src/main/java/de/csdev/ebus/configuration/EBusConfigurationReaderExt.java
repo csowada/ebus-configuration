523d669db8d7bc18d816b2916b6a7a97d5b05b6c
Christian S <git@cs-dev.de>
2018-01-24 20:04:55 +0100

    Add errorhandler to constructor

--- src/main/java/de/csdev/ebus/configuration/EBusConfigurationReaderExt.java
+++ src/main/java/de/csdev/ebus/configuration/EBusConfigurationReaderExt.java
@@ -2,17 +2,25 @@ package de.csdev.ebus.configuration;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.csdev.ebus.cfg.std.EBusConfigurationReader;
 import de.csdev.ebus.command.IEBusCommandCollection;
 
 public class EBusConfigurationReaderExt extends EBusConfigurationReader {
 
+    private static final Logger logger = LoggerFactory.getLogger(EBusConfigurationReaderExt.class);
+
     @Override
     public List<IEBusCommandCollection> loadBuildInConfigurationCollections() {
-        // TODO Auto-generated method stub
-        // return super.loadBuildInConfigurationCollections();
-        return loadConfigurationCollectionBundle(
-                EBusConfigurationReaderExt.class.getResource("/index-configuration.json"));
+        try {
+            return loadConfigurationCollectionBundle(
+                    EBusConfigurationReaderExt.class.getResource("/index-configuration.json"));
+        } catch (Exception e) {
+            logger.error("error!", e);
+        }
+        return null;
     }
 
 }
