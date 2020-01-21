4400d19964ac72990c9438f5bb6eaf56de09de2b
Christian S <git@cs-dev.de>
2020-01-21 21:44:41 +0100

    Add Version class to read new MANIFETS attributes

---
+++ src/main/java/de/csdev/ebus/configuration/EBusConfigurationVersion.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2016-2020 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package de.csdev.ebus.configuration;
+
+import de.csdev.ebus.core.EBusVersion;
+
+public class EBusConfigurationVersion extends EBusVersion {
+
+    public static String getVersion() {
+        return getVersion(EBusConfigurationVersion.class);
+    }
+
+    public static String getBuildCommit() {
+        return getBuildCommit(EBusConfigurationVersion.class);
+    }
+
+    public static String getBuildTimestamp() {
+        return getBuildTimestamp(EBusConfigurationVersion.class);
+    }
+
+    public static String getBuildNumber() {
+        return getBuildNumber(EBusConfigurationVersion.class);
+    }
+}
